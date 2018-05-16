a85d371883cfae03069ebaf909cdd036476c5e6e
Luke Chambers <lukenatchambers@gmail.com>
2018-05-16 10:29:30 -0600

    Initial commit #2

--- lib/linked_list.ex
+++ lib/linked_list.ex
@@ -27,4 +27,22 @@ defmodule LinkedList do
   def head(node) do
     node
   end
+
+  def append(value) do
+    cond do
+      @head == nil ->
+        @head(Node.new(value))
+      @head != nil ->
+        traverse_and_place(value)
+    end
+  end
+
+  def traverse_and_place(value, current_position \\ @head) do
+    cond do
+      current_position.next_node == nil ->
+        current_position.next_node(Node.new(value))
+      current_position.next_node != nil ->
+        traverse_and_place(value, current_position.next_node)
+    end
+  end
 end
